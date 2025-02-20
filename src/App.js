d1f4012812462335933cb74bb8ac8bbb4d58bca9
unknown <buivanluu1997@gmail.com>
2025-02-20 08:18:18 +0700

    luu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import HeaderComponent from "./component/HeaderComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
+import {Route, Routes} from "react-router-dom";
+import MedicalBooking from "./component/MedicalBooking";
 
 function App() {
     return (
         <>
             <HeaderComponent/>
+            <Routes>
+                <Route path="/booking" element={<MedicalBooking/>}/>
+            </Routes>
         </>
     );
 }
