8dc392aa0f5ebcfa12379adcf5cbe83a08db6e75
unknown <buivanluu1997@gmail.com>
2025-02-24 07:55:49 +0700

    luu

--- src/component/booking/MedicalInfo.jsx
+++ src/component/booking/MedicalInfo.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import imageBooking from '../../assets/images/image-booking.png'
+import khamBenh from '../../assets/images/kham_benh.jpg'
 export default function MedicalInfo(){
     return(
         <>
             <div className="mt-10">
                 <img src={imageBooking}
+                     alt="Ảnh khách hàng"
+                     className="w-[1200px] h-[330px] object-cover rounded-lg shadow-lg"/>
+            </div>
+            <div className="mt-10">
+                <img src={khamBenh}
                      alt="Ảnh khách hàng"
                      className="w-[1200px] h-[330px] object-cover rounded-lg shadow-lg"/>
             </div>
--- src/component/sections/Facilities.jsx
+++ src/component/sections/Facilities.jsx
@@ -24,7 +24,7 @@ export const Facilities = () => {
                             xét nghiệm để đảm bảo độ chính xác và an toàn cao.
                         </p>
                     </div>
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Xem chi tiết</button>
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Xem chi tiết</button>
                 </div>
 
                 <div className="lg:w-1/2">
